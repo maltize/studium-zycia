a79b52bcd72f5a58bde746993602490dc65554ad
Maciej <maltize@gmail.com>
2010-10-19 23:10:17 +0200

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudiumZycia::Application.config.secret_token = '3488c0be3d7f098806395241f9be3acd81d1779d1cf0c2045c9f105483c3de6e5191cefe887889bf0f88cc5c4d0158b055cee17455e5cb66f7c61856261fceaa'
