54549afe188596cd7e8553696c95e4875ec1e285
Maciej <maltize@gmail.com>
2010-11-03 23:43:54 +0100

    updated mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,11 @@ class UserMailer < ActionMailer::Base
   FEEDBACK_EMAIL = "jacek@studium-zycia.pl"
 
   def feedback_email(feedback)
-    @feedback = feedback
     mail(:from => feedback.username, :to => FEEDBACK_EMAIL,
       :subject => "Studium-Zycia.pl - Wiadomość z serwisu",
-      :content_type => "multipart/alternative")
+      :content_type => "multipart/alternative") do |format|
+      format.text { render :text => "Wiadomość od #{feedback.username}\n\n#{feedback.message}" }
+    end
   end
 
 end
